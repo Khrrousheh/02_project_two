60d954369e5af9dab88b0a5b062b4597789b0c0c
Mahdi Khrrousheh <mahdi@megit.com>
2024-07-20 01:10:53 +0300

    url: section-three lessson 27

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, \
+    HttpResponseRedirect
 
-# Create your views here.
+challenges_map = {
+    'january': "Reading Challenge",
+    'february': "Acts of Kindness Challenge",
+    'march': "Fitness Challenge",
+    'april': "Hydration Challenge",
+    'may': "Art Challenge",
+    'june': "Digital Detox Challenge",
+    'july': "Decluttering Challenge",
+    'august': "Gratitude Challenge",
+    'september': "Skill-building Challenge",
+    'november': "Financial Challenge",
+    'december': "Self-care Challenge",
+}
+
+
+def monthly_challenge_by_number(request, month: int):
+    try:
+        months = list(challenges_map.keys())
+        redirect_month = months[month - 1]
+        return HttpResponseRedirect('/challenges/' + redirect_month)
+    except Exception as e:
+        return HttpResponseNotFound(e)
+
+def monthly_challenge(request, month: str):
+    try:
+        msg = challenges_map.get(month.lower())
+        return HttpResponse(msg)
+    except Exception as e:
+        print(e)
+        return HttpResponseNotFound('This month is not supported!')
