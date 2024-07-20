c942aee6dadb8f6da1e3d58ef0c06ca145e19dff
Mahdi Khrrousheh <mahdi@megit.com>
2024-07-21 00:26:56 +0300

    finish section 3

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, \
     HttpResponseRedirect
+from django.urls import reverse
 
 challenges_map = {
     'january': "Reading Challenge",
@@ -16,18 +17,32 @@ challenges_map = {
 }
 
 
+def index(request):
+    msg_body = '<ul>'
+    months = list(challenges_map.keys())
+    for idx, name in enumerate(months):
+        redirect_month = months[idx]
+        redirect_path = reverse('month-challenge', args=[redirect_month])
+        msg_body += f"\n<li><a href={redirect_path}>name</a></li>"
+    msg_body += '</ul>'
+    return HttpResponse(msg_body)
+
+
 def monthly_challenge_by_number(request, month: int):
     try:
         months = list(challenges_map.keys())
         redirect_month = months[month - 1]
-        return HttpResponseRedirect('/challenges/' + redirect_month)
+        redirect_path = reverse('month-challenge', args=[redirect_month])
+        return HttpResponseRedirect(redirect_path)
     except Exception as e:
         return HttpResponseNotFound(e)
+
 
 def monthly_challenge(request, month: str):
     try:
         msg = challenges_map.get(month.lower())
-        return HttpResponse(msg)
+        msg_html = f"<h1>{msg}</h1>"
+        return HttpResponse(msg_html)
     except Exception as e:
         print(e)
         return HttpResponseNotFound('This month is not supported!')
